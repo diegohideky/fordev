abef82b28fe8320fa3b6f1927a35c37a26c08189
Diego Hideky <diegohideky@gmail.com>
2021-01-13 08:26:34 -0300

    Added test to validate body in httpclient

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:faker/faker.dart';
 import 'package:http/http.dart';
 import 'package:mockito/mockito.dart';
@@ -16,20 +18,37 @@ class HttpAdapter {
     @required String method,
     Map body
   }) async {
-    await client.post(url);
+    final headers = {
+      'content-type': 'application/json',
+      'accept': 'application/json'
+    };
+    await client.post(url, headers: headers, body: jsonEncode(body));
   }
 }
 
-void main() {  
+void main() {
+  HttpAdapter sut;
+  ClientSpy client;
+  String url;
+
+  setUp(() {
+    client = ClientSpy();
+    sut = HttpAdapter(client);
+    url = faker.internet.httpUrl();
+  });
+
   group('post', () {
     test('Should call post with correct values', () async {
-      final client = ClientSpy();
-      final sut = HttpAdapter(client);
-      final url = faker.internet.httpUrl();
+      await sut.request(url: url, method: 'post', body: {'any_key': 'any_value'});
 
-      await sut.request(url: url, method: 'post');
-
-      verify(client.post(url));
+      verify(client.post(
+        url,
+        headers: {
+          'content-type': 'application/json',
+          'accept': 'application/json'
+        },
+        body: '{"any_key":"any_value"}'
+      ));
     });
   });
 }
